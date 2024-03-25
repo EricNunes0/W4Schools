85d988c6cf68eebaf8712fbd21f8b719c70cfb73
Eric Nunes Ramalho <eric26052004@gmail.com>
2024-03-25 14:19:01 -0300

    Versão 0.6.5.21 - HTML Website (Parte 2)

--- src/pages/Sections/html/website/Website.jsx
+++ src/pages/Sections/html/website/Website.jsx
@@ -68,6 +68,21 @@ export default function Website() {
                     <path id="wavepath1" fill-rule="evenodd" clip-rule="evenodd" d="M0 293.923V393.09H330.556H1983.33H3966.67L5950 393.091L5950 203.02C5288.89 244.34 4627.78 194.756 3966.67 128.645C3878.52 119.83 3790.37 110.722 3702.22 101.613C3129.26 42.4069 2556.3 -16.7992 1983.33 4.6869C1322.22 29.4786 661.111 161.701 330.556 227.812L0 293.923Z" fill="#1d2a35"></path>
                 </svg>
             </div>
+            <div id="html-website-section-third">
+                <div id="html-website-section-third-content">
+                    <div id="html-website-section-third-content-top">
+                        <h1 className="html-website-section-third-content-top-h1">Prática leva a perfeição</h1>
+                        <h4 className="html-website-section-third-content-top-h4">Aprimore e domine suas habilidades de desenvolvimento com essas tecnologias usando o <b>Spaces</b>.</h4>
+                        <img id="html-website-section-third-content-top-img" src="https://www.w3schools.com/spaces/languages.png" alt="Languages"></img>
+                        <div className="display-flex-row">
+                            <a id="html-website-section-third-content-top-spaces-button" href="">Inicie gratuitamente</a>
+                        </div>
+                    </div>
+                </div>
+                <svg style={{width: "100%", marginBottom: "-10px"}} viewBox="0 0 5950 364" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path id="wavepath2" fill-rule="evenodd" clip-rule="evenodd" d="M141.726 0H0V363.61H5950L5950 181.806C5795.94 166.176 5642.96 136.068 5489.96 105.955C5359.82 80.3423 5229.67 54.7264 5098.82 38.0139C4817.85 3.93391e-06 4533.16 3.93391e-06 4251.77 38.0139C4120 54.8193 3988.58 80.6274 3857.44 106.382C3704.83 136.352 3552.59 166.25 3400.59 181.806C3115.49 210.729 2833.27 198.333 2549.41 166.517C2411.5 151.712 2272.59 133.223 2133.17 114.667C1988.55 95.4186 1843.39 76.0974 1698.23 60.7396C1417.26 28.9236 1133.39 16.5278 851.18 7.4375C566.076 0 283.452 0 141.726 0H141.726Z" fill="#D9EEE1"></path>
+                </svg>
+            </div>
         </section>
     );
 };
