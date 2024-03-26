5149bbae2623848ddfa0c34a9751fc1670963526
Eric Nunes Ramalho <eric26052004@gmail.com>
2024-03-26 14:47:03 -0300

    Versão 0.6.5.22 - HTML Website (Parte 3)

--- src/pages/Sections/html/website/Website.jsx
+++ src/pages/Sections/html/website/Website.jsx
@@ -3,9 +3,9 @@ import WebsiteStyle from "./WebsiteStyle.css";
 export default function Website() {
     return (
         <section id="html-website-section">
-            <div id="html-website-section-first">
-                <div id="html-website-section-first-content">
-                    <div id="html-website-section-first-content-top">
+            <div className="html-website-sections" id="first">
+                <div className="html-website-section-contents">
+                    <div className="html-website-section-content-tops" id="first">
                         <a href="" title="O que é o AI?"><img id="ai-ribbon" src="https://www.w3schools.com/spaces/kai_ribbon.png"></img></a>
                         <h1 id="html-website-section-first-content-top-title">Crie seu próprio site com HTML</h1>
                         <h4 id="html-website-section-first-content-top-details-description">Hospede sites com <span style={{color: "#FFC0C7"}}>W3Schools Spaces</span></h4>
@@ -56,9 +56,9 @@ export default function Website() {
                     <path id="wavepath0" fill-rule="evenodd" clip-rule="evenodd" d="M0 293.923V393.09H330.556H1983.33H3966.67L5950 393.091L5950 203.02C5288.89 244.34 4627.78 194.756 3966.67 128.645C3878.52 119.83 3790.37 110.722 3702.22 101.613C3129.26 42.4069 2556.3 -16.7992 1983.33 4.6869C1322.22 29.4786 661.111 161.701 330.556 227.812L0 293.923Z" fill="#D9EEE1"></path>
                 </svg>
             </div>
-            <div id="html-website-section-second">
-                <div id="html-website-section-second-content">
-                    <div id="html-website-section-second-content-top">
+            <div className="html-website-sections" id="second">
+                <div className="html-website-section-contents">
+                    <div className="html-website-section-content-tops" id="second">
                         <h1 className="html-website-section-second-content-top-h1">Editor de código poderoso</h1>
                         <h5 className="html-website-section-second-content-top-h5">Front-end, Back-end ou Full-stack - a escolha é sua</h5>
                         <img id="html-website-section-second-content-top-img" src="https://www.w3schools.com/spaces/dynamicspaces.gif" alt="Dynamic Spaces" loading="lazy"></img>
@@ -68,9 +68,9 @@ export default function Website() {
                     <path id="wavepath1" fill-rule="evenodd" clip-rule="evenodd" d="M0 293.923V393.09H330.556H1983.33H3966.67L5950 393.091L5950 203.02C5288.89 244.34 4627.78 194.756 3966.67 128.645C3878.52 119.83 3790.37 110.722 3702.22 101.613C3129.26 42.4069 2556.3 -16.7992 1983.33 4.6869C1322.22 29.4786 661.111 161.701 330.556 227.812L0 293.923Z" fill="#1d2a35"></path>
                 </svg>
             </div>
-            <div id="html-website-section-third">
-                <div id="html-website-section-third-content">
-                    <div id="html-website-section-third-content-top">
+            <div className="html-website-sections" id="third">
+                <div className="html-website-section-contents">
+                    <div className="html-website-section-content-tops" id="third">
                         <h1 className="html-website-section-third-content-top-h1">Prática leva a perfeição</h1>
                         <h4 className="html-website-section-third-content-top-h4">Aprimore e domine suas habilidades de desenvolvimento com essas tecnologias usando o <b>Spaces</b>.</h4>
                         <img id="html-website-section-third-content-top-img" src="https://www.w3schools.com/spaces/languages.png" alt="Languages"></img>
@@ -81,6 +81,39 @@ export default function Website() {
                 </div>
                 <svg style={{width: "100%", marginBottom: "-10px"}} viewBox="0 0 5950 364" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path id="wavepath2" fill-rule="evenodd" clip-rule="evenodd" d="M141.726 0H0V363.61H5950L5950 181.806C5795.94 166.176 5642.96 136.068 5489.96 105.955C5359.82 80.3423 5229.67 54.7264 5098.82 38.0139C4817.85 3.93391e-06 4533.16 3.93391e-06 4251.77 38.0139C4120 54.8193 3988.58 80.6274 3857.44 106.382C3704.83 136.352 3552.59 166.25 3400.59 181.806C3115.49 210.729 2833.27 198.333 2549.41 166.517C2411.5 151.712 2272.59 133.223 2133.17 114.667C1988.55 95.4186 1843.39 76.0974 1698.23 60.7396C1417.26 28.9236 1133.39 16.5278 851.18 7.4375C566.076 0 283.452 0 141.726 0H141.726Z" fill="#D9EEE1"></path>
+                </svg>
+            </div>
+            <div className="html-website-sections" id="fourth">
+                <div className="html-website-section-contents">
+                    <div className="html-website-section-content-tops" id="fourth">
+                        <h2 className="html-website-section-fourth-content-top-h2">Economize tempo usando modelos prontos</h2>
+                        <h4 className="html-website-section-fourth-content-top-h4">Crie sites poderosos com apenas alguns cliques</h4>
+                    </div>
+                    <div className="html-website-section-templates">
+                        <div className="html-website-section-template-items">
+                            <img className="html-website-section-template-items-img" src="https://www.w3schools.com/spaces/portfolio_template.png" alt="Gamer website template"></img><br></br>
+                            <a className="html-website-section-template-items-a" href="https://spaces.w3schools.com/createspace?template=blackandwhiteportfolio"><b>Portfolio</b></a> <br></br>
+                        </div>
+                        <div className="html-website-section-template-items">
+                            <img className="html-website-section-template-items-img" src="https://www.w3schools.com/spaces/blog_template.png" alt="Blog website template"></img><br></br>
+                            <a className="html-website-section-template-items-a" href="https://spaces.w3schools.com/createspace?template=node-handlebars-blog"><b>Blog</b></a> <br></br>
+                        </div>
+                    </div>
+                    <div className="html-website-section-templates">
+                        <div className="html-website-section-template-items">
+                            <img className="html-website-section-template-items-img" src="https://www.w3schools.com/spaces/webshop_template.png" alt="AI website template"></img><br></br>
+                            <a className="html-website-section-template-items-a" href="https://spaces.w3schools.com/createspace?template=node-none-webshop"><b>Webshop</b></a> <br></br>
+                        </div>
+                        <div className="html-website-section-template-items">
+                            <img className="html-website-section-template-items-img" src="https://www.w3schools.com/spaces/tutor_template.png" alt="Tutor website template"></img><br></br>
+                            <a className="html-website-section-template-items-a" href="https://spaces.w3schools.com/createspace?template=node-react-the_tutor"><b>Tutor</b></a> <br></br>
+                        </div>
+                    </div>
+                    <a className="html-website-section-template-items-a" href="https://spaces.w3schools.com/createspace">Explore All Templates</a> <br></br>
+                    <a className="html-website-section-template-items-a black" href="https://profile.w3schools.com/sign-up?redirect_url=https%3A%2F%2Fspaces.w3schools.com%2F">Get Started for Free</a>
+                </div>
+                <svg style={{width: "100%", marginBottom: "-10px"}} viewBox="0 0 5950 410" fill="none" xmlns="http://www.w3.org/2000/svg" id="kai-landed">
+                    <path id="wavepath3" fill-rule="evenodd" clip-rule="evenodd" d="M141.726 45.4514L0 0V409.063H140.486H851.18H1698.23H2549.41H3400.59H4251.77H5098.82H5950V189.243C5938.41 191.46 5926.82 193.683 5915.25 195.905C5642.01 248.332 5371.94 300.154 5098.82 265.271C4981.35 249.378 4863.23 216.296 4744.97 183.176C4580.38 137.076 4415.51 90.9028 4251.77 90.9028C4061.21 90.9028 3871.39 151.819 3682.05 212.577C3588.12 242.721 3494.31 272.826 3400.59 295.434C3115.49 363.611 2833.27 363.611 2549.41 318.16C2445.26 301.311 2340.53 278.217 2235.46 255.045C2057.07 215.705 1877.65 176.138 1698.23 166.517C1557.84 159.704 1416.73 170.738 1275.46 181.783C1134 192.843 992.382 203.916 851.18 197.094C567.058 181.858 285.398 91.5281 143.195 45.9227L141.726 45.4514Z" fill="#282A35"></path>
                 </svg>
             </div>
         </section>
