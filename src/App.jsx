7071cc60d7a93e1663ac8133c9ef3797e2413753
Eric Nunes Ramalho <eric26052004@gmail.com>
2024-07-20 23:06:14 -0300

    Versão 0.9.0.11 - CSS Ícones

--- src/App.jsx
+++ src/App.jsx
@@ -3796,6 +3796,10 @@ function App() {
 							<Navigate to = ""></Navigate>
 						)}></Route>
 					</Route>
+					{/* Ícones */}
+					<Route path = "icons" element = {(
+						<Page content = {(<TutorialMain json = "css" language = "css" topic = "CSS Tutorial" title = "CSS Ícones"></TutorialMain>)}></Page>
+					)}></Route>
 				</Route>
 				<Route path = "*" element = {(
 					<NotFound></NotFound>
